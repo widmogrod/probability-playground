a0bb5b405ebf55432b87f55db6b8bc4383e4000a
widmogrod <widmogrod@gmail.com>
2020-03-15 18:24:20 +0100

    Simulation of many degrees of freedom
    Probability of reaching consensus of certain degrees, when number of independent decisions per tasks increases

--- README.md
+++ README.md
@@ -10,5 +10,22 @@ This capability is also known under the name of crowdsourcing.
 Because is hard to verify the quality of work product in such setup, the question arises: 
 **What is the probability that in n-tasks workers deciding on random, will reach consensus on the same decision?**
 
-To find an answer to this question please take a look at example [example/aws_mechanical_turk_probability_of_consensus_monte_carlo_test.go](example/aws_mechanical_turk_probability_of_consensus_monte_carlo_test.go)
+This test case aims to calculate probability of random answers where:
+- there is n-tasks to be solved by workers
+- each worker can make one of four decision per task (independent, with identical probability)
+- each tasks has to be answer k-times, by different workers
 
+To measure the probability of agreement on decision let's introduce the concept called the degree of consensus:
+
+> `n-degree consensus` = n-times the same decision was selected by k-number of independent workers. 
+
+Example degrees and their interpretations
+- consensus of degree 3 - happens when three independent workers make the same decision on one task
+- consensus of degree 2 - happens when two independent workers choose the same decision, but one makes different
+- consensus of degree 1 - happens when each worker makes different decision
+
+Bellow you can see how probability of reaching different degrees of consensus (y axis) changes when more number of answers is collected per task (x axis)
+![Probability of reaching consensus in AWS mTurk](./example/aws_mechanical_turk_probability_of_consensus_monte_carlo_test.png)
+Figure: Probability of reaching consensus of certain degrees, when number of independent decisions per tasks increases
+
+To understand how this is implemented please take a look at example [example/aws_mechanical_turk_probability_of_consensus_monte_carlo_test.go](example/aws_mechanical_turk_probability_of_consensus_monte_carlo_test.go)
--- example/aws_mechanical_turk_probability_of_consensus_monte_carlo_test.go
+++ example/aws_mechanical_turk_probability_of_consensus_monte_carlo_test.go
@@ -1,7 +1,13 @@
 package example
 
 import (
+	"golang.org/x/exp/errors/fmt"
+	"gonum.org/v1/plot"
+	"gonum.org/v1/plot/plotter"
+	"gonum.org/v1/plot/plotutil"
+	"gonum.org/v1/plot/vg"
 	"math/rand"
+	"strconv"
 	"testing"
 )
 
@@ -15,7 +21,7 @@ import (
 // - each worker can make one of four decision per task (independent, with identical probability)
 // - each tasks has to be answer k-times, by different workers
 //
-// To measure probability of agreement on decision measure of consensus is introduced
+// To measure the probability of agreement on decision let's introduce the concept called the degree of consensus:
 // - consensus of degree 3 - happens when three independent workers make the same decision on one task
 // - consensus of degree 2 - happens when two independent workers choose the same decision, but one makes different
 // - consensus of degree 1 - happens when each worker makes different decision
@@ -23,47 +29,111 @@ func TestAWSMechanicalTurkProbabilityOfConsensusMonteCarlo(t *testing.T) {
 	n := 10000
 	k := 3
 	epsilon := 0.01
-	consensusDegree3 := .0
-	consensusDegree2 := .0
-	consensusDegree1 := .0
+
+	consensusProbability := mTurkMonteCarlo(n, k)
+
+	between(t, consensusProbability[3], 0.06, 0.08, epsilon)
+	between(t, consensusProbability[2], 0.53, 0.55, epsilon)
+	between(t, consensusProbability[1], 0.35, 0.38, epsilon)
+}
+
+// mTurkMonteCarlo simulation where
+// - n-tasks to be solved by workers
+// - each tasks has to be answer k-times
+func mTurkMonteCarlo(n, k int) []float64 {
+	consensusDegrees := make([]float64, k+1)
+	consensusProbability := make([]float64, k+1)
 
 	// there is n tasks to be solved by workers,
 	for i := 0; i < n; i++ {
-		voteA, voteB, voteC, voteD := 0, 0, 0, 0
+		votes := make([]float64, 4)
 		// each task must be answer k-times
 		for w := 0; w < k; w++ {
 			decision := rand.Float32()
 			if decision <= 0.25 {
-				voteA++
+				votes[0]++
 			} else if decision <= 0.5 {
-				voteB++
+				votes[1]++
 			} else if decision <= 0.75 {
-				voteC++
+				votes[2]++
 			} else {
-				voteD++
+				votes[3]++
 			}
 		}
 
-		// when workers agree 3 times on the same decision we have consensus of degree 3
-		if voteA >= 3 || voteB >= 3 || voteC >= 3 || voteD >= 3 {
-			consensusDegree3++
-		} else if voteA >= 2 || voteB >= 2 || voteC >= 2 || voteD >= 2 {
-			// when workers agree 2 times on the same decision we have consensus of degree 2
-			consensusDegree2++
-		} else {
-			// when each worker makes different decisions, then it's consensus of degree 1
-			consensusDegree1++
-		}
+		consensusReached := int(max(votes))
+		consensusDegrees[consensusReached]++
 	}
 
 	// What is probability of degree 3,2 and 1 consensus?
 	// or in other words:
 	// What is probability that in n-tasks workers making decision on random, will reach consensus on the same decision?
-	degree3Probability := consensusDegree3 / float64(n)
-	degree2Probability := consensusDegree2 / float64(n)
-	degree1Probability := consensusDegree1 / float64(n)
+	for degree, reachTimes := range consensusDegrees {
+		consensusProbability[degree] = reachTimes / float64(n)
+	}
 
-	between(t, degree3Probability, 0.06, 0.08, epsilon)
-	between(t, degree2Probability, 0.53, 0.55, epsilon)
-	between(t, degree1Probability, 0.35, 0.38, epsilon)
+	return consensusProbability
+}
+
+func max(xs []float64) float64 {
+	result := .0
+	for _, x := range xs {
+		if x > result {
+			result = x
+		}
+	}
+
+	return result
+}
+
+func TestPlotDistributionOfAWSMechanicalTurkProbabilityOfConsensusMonteCarlo(t *testing.T) {
+	rand.Seed(int64(0))
+
+	p, err := plot.New()
+	if err != nil {
+		panic(err)
+	}
+
+	p.Title.Text = "mTurn degrees of consensus"
+	p.X.Label.Text = "decisions per task"
+	p.Y.Label.Text = "probability of degree of consensus"
+	p.Legend.Top = true
+
+	n := 20000
+	maxWorkers := 37
+
+	degrees := make([]interface{}, maxWorkers*2)
+	for workers := 0; workers < maxWorkers; workers++ {
+		degrees[workers*2] = fmt.Sprintf("Degree %d", workers)
+		degrees[workers*2+1] = make(plotter.XYs, maxWorkers)
+	}
+
+	for workers := 0; workers < maxWorkers; workers++ {
+		consensusProbability := mTurkMonteCarlo(n, workers)
+		for degree, probability := range consensusProbability {
+			if d, ok := degrees[degree*2+1].(plotter.XYs); ok {
+				degrees[degree*2+1] = append(
+					d,
+					plotter.XY{float64(workers), probability},
+				)
+			}
+		}
+	}
+
+	err = plotutil.AddLinePoints(p, degrees...)
+	if err != nil {
+		panic(err)
+	}
+
+	labels := []string{}
+	for i := 0; i < maxWorkers; i++ {
+		labels = append(labels, strconv.Itoa(i))
+	}
+
+	p.NominalX(labels...)
+
+	// Save the plot to a PNG file.
+	if err := p.Save(18*vg.Inch, 9*vg.Inch, "aws_mechanical_turk_probability_of_consensus_monte_carlo_test.png"); err != nil {
+		panic(err)
+	}
 }
