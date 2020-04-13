2fcf90256f56162abd9698c9c049ee88561c6f98
widmogrod <widmogrod@gmail.com>
2020-04-13 15:16:01 +0200

    Refactor binomial distribution example

--- example/binomial_distribution_test.go
+++ example/binomial_distribution_test.go
@@ -12,6 +12,11 @@ import (
 	"testing"
 )
 
+func binomialDistribution(n, k int, p float64) float64 {
+	// f(n,k,p) = (n choose k) p^k (1-p)^(n-k)
+	return float64(combin.Binomial(n, k)) * math.Pow(p, float64(k)) * math.Pow(1.0-p, float64(n-k))
+}
+
 func TestPlotBinomialDistribution(t *testing.T) {
 	rand.Seed(int64(0))
 
@@ -19,6 +24,11 @@ func TestPlotBinomialDistribution(t *testing.T) {
 	if err != nil {
 		panic(err)
 	}
+
+	p.Title.Text = "Probability of n successes in n trails, where probability of success p is"
+	p.X.Label.Text = "n - number of trials"
+	p.Y.Label.Text = "probability of success in n - trails"
+	p.Legend.Top = true
 
 	lines := make([]interface{}, 0)
 	for i := 0; i <= 20; i++ {
@@ -35,33 +45,10 @@ func TestPlotBinomialDistribution(t *testing.T) {
 		lines = append(lines, label, points)
 	}
 
-	p.Title.Text = "Probability of n successes in n trails, where probability of success p is"
-	p.X.Label.Text = "n - number of trials"
-	p.Y.Label.Text = "probability of success in n - trails"
-	p.Legend.Top = true
 	err = plotutil.AddLinePoints(p, lines...)
-
-	//fmt.Println(rangeFromToBy(1, 100, 1, "%f.0"))
-	//p.NominalX(rangeFromToBy(1, 100, 1, "%d")...)
-	//p.NominalY(rangeFromToBy(0, 1, 0.1, "%f")...)
 
 	// Save the plot to a PNG file.
 	if err := p.Save(18*vg.Inch, 9*vg.Inch, "binomial_distribution_test.png"); err != nil {
 		panic(err)
 	}
-}
-
-func rangeFromToBy(start, stop, step float64, label string) []string {
-	l := int((stop - start) / step)
-	result := make([]string, l)
-	for i := start; i <= stop; i += step {
-		result = append(result, fmt.Sprintf(label, i))
-	}
-
-	return result
-}
-
-func binomialDistribution(n, k int, p float64) float64 {
-	// f(n,k,p) = (n choose k) p^k (1-p)^(n-k)
-	return float64(combin.Binomial(n, k)) * math.Pow(p, float64(k)) * math.Pow(1.0-p, float64(n-k))
 }
